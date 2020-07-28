ee55ef3e08da96ed7bd32d9feb2e1afb1ab379a5
Oleksandr Shevtsov <oleksandr.shevtsov@90poe.io>
2020-07-28 18:30:40 +0300

    feat: logger and lifecycle for client_api

--- client/main.go
+++ client/main.go
@@ -1,43 +1,35 @@
 package main
 
 import (
-	"log"
-	"net/http"
+	"os"
 
-	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/Shelex/grpc-go-demo/client/graph"
-	"github.com/Shelex/grpc-go-demo/client/graph/generated"
+	client "github.com/Shelex/grpc-go-demo/client/service"
 	"github.com/Shelex/grpc-go-demo/config"
-	"github.com/Shelex/grpc-go-demo/proto"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
+	"github.com/Shelex/grpc-go-demo/logger"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	cfg := config.GetEnv()
-
-	creds, err := credentials.NewClientTLSFromFile(cfg.PathToTLSCertFile, "")
-	if err != nil {
-		log.Fatal(err)
-	}
-	opts := []grpc.DialOption{grpc.WithTransportCredentials(creds)}
-	conn, err := grpc.Dial(cfg.Host+":"+cfg.DomainServicePort, opts...)
-	if err != nil {
-		log.Fatal(err)
-	}
 	defer func() {
-		if err := conn.Close(); err != nil {
-			log.Fatalf("failed to close connection: %s", err)
+		if r := recover(); r != nil {
+			log.Fatal(r)
 		}
 	}()
-	client := proto.NewEmployeeServiceClient(conn)
 
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: graph.NewResolver(client)}))
+	cfg := config.GetEnv()
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	logger.Init(cfg)
+	log.Info("starting client...")
 
-	log.Printf("connect to http://%s:%s/ for GraphQL playground", cfg.Host, cfg.ClientAPIPort)
-	log.Fatal(http.ListenAndServe(":"+cfg.ClientAPIPort, nil))
+	conn, err := client.ConnectGRPCService(cfg)
+	if err != nil {
+		log.Errorf("failed to create connection to server: %s", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	if err := client.Start(cfg, conn); err != nil {
+		log.Errorf("failed to start employee client: %s", err)
+		os.Exit(1)
+	}
 }
